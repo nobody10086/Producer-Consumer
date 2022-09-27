92bcc3e7a1787d798f2b935ce846326a465e08ce
Mr.Nobody <66677375+nobody10086@users.noreply.github.com>
2022-09-27 17:08:56 +0800

    Initial commit

---
+++ src/Storage.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+
+public class Storage {
+    private final int max_StorageSize = 10;
+
+    //仓库存储的载体
+    private LinkedList<Object> list = new LinkedList<>();
+    
+}
